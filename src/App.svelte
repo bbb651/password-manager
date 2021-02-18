13a70e5208e346fd3b8ff60a47c5744c6e4108a4
bbb651 <bar.ye651@gmail.com>
2021-02-18 14:51:08 +0200

    Removed hardcoded entries (and an unused import)

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,9 @@
 <script lang="ts">
 	import PasswordTable from "./PasswordTable.svelte";
-	import PasswordFrom from "./PasswordForm.svelte";
+	import PasswordForm from "./PasswordForm.svelte";
 	import type { Password } from "./types/Password";
-import PasswordForm from "./PasswordForm.svelte";
-	let passwords: Password[] = [
-		{password: "p4ssword", username: "coolkid72", title: "Google", url:"google.com", notes: "some notes"},
-		{password: "p4SSword", username: "coolkid72", title: "Github", url:"github.com", notes: ""},
-		{password: "goodp4ssword", username: "coolkid", title: "Discord", url:"discord.com", notes: "Requires 2FA"},
-		{password: "stronGp4ssWORD", username: "coolkid72005", title: "Steam", url:"https://store.steampowered.com/", notes: ""},
-		{password: "badpassword", username: "coolkid7", title: "Zoom", url:"zoom.us", notes: ""},
-	];
+
+	let passwords: Password[] = [];
 
 	const addPassword = (password: Password) => {
 		passwords = [...passwords, password];
