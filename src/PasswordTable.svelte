796dd78edab48669e87abf80d5e5b819d31c3795
bbb651 <bar.ye651@gmail.com>
2021-02-18 11:08:14 +0200

    Password copying support with bootstrap icon

--- src/PasswordTable.svelte
+++ src/PasswordTable.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
 	import type { Password } from "./types/Password";
 	export let passwords: Password[];
+
+	const copy = (e: MouseEvent) => {
+		const entry: HTMLElement = (e.target as HTMLElement).parentElement;
+		navigator.clipboard.writeText(entry.children[1].textContent);
+	};
 </script>
 
 <table>
@@ -14,7 +19,9 @@
 	{#each passwords as password}
 		<tr>
 			<td>{password.title ?? ""}</td>
-			<td>{password.password}</td>
+			<td>
+				{password.password}
+			</td>
 			<td>{password.username ?? ""}</td>
 			<td
 				>{#if password.url}
@@ -27,6 +34,22 @@
 				{/if}</td
 			>
 			<td>{password.notes ?? ""}</td>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="16"
+				height="16"
+				fill="currentColor"
+				class="bi bi-clipboard"
+				viewBox="0 0 16 16"
+				on:click={copy}
+			>
+				<path
+					d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"
+				/>
+				<path
+					d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"
+				/>
+			</svg>
 		</tr>
 	{/each}
 </table>
@@ -34,7 +57,12 @@
 <style>
 	table {
 		margin-left: auto;
-  		margin-right: auto;
+		margin-right: auto;
 		border-spacing: 25px;
+	}
+
+	.bi {
+		display: inline-block;
+		vertical-align: middle;
 	}
 </style>
